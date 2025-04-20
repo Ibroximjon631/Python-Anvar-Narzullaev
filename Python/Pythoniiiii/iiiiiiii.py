82e98180bb0a244809fd8ef7c6727b83b0e5cfdd
Poochie Brigitte <poochieb@an-i4.tash.21-school.uz>
2025-04-20 11:52:27 +0500

    Abcdef

---
+++ Python/Pythoniiiii/iiiiiiii.py
@@ -0,0 +1,27 @@
+iii="Ibroximjon"
+iiii=17
+abc=3.7
+print(type(iii),type(iiii),type(abc))
+print(f"Hello {iii} {iiii}")
+print("Hello \tIbroximjon")
+print("Hello \nIbroximjon")
+abcd="Python iii"
+print(abcd.upper())
+print(abcd.lower())
+print(abcd.title())
+print(abcd.capitalize())
+
+
+
+
+# Bosh joyt...
+Abc="   Ibroximjon  "
+print(Abc.lstrip())
+print(Abc.rstrip())
+print(Abc.strip())
+print(Abc)
+
+
+
+Name=input("Name:?")
+print(Name.title())
