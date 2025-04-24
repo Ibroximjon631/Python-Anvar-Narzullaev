7730e4d7f29d3807b58ed51084c5c8a29e7842b2
ibroximjon631 <ibroximjon631@gmail.com>
2025-04-24 17:30:01 +0500

    iii

---
+++ Python/Pythoniiiii/Dict.py
@@ -0,0 +1,9 @@
+dicttt={
+    'name':'Ibroximjon',
+    'age':20,
+    'nimadr':'Iiii'
+}
+#print(dicttt.items())
+for ii,iii in dicttt.items():
+    print(f"Ii: {ii},Iii: {iii}")
+print(dicttt.keys())
