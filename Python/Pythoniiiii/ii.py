a73c75d1870e74ca329dd574599a3961ce1c629a
ibroximjon631 <ibroximjon631@gmail.com>
2025-04-24 16:34:53 +0500

    Abc

---
+++ Python/Pythoniiiii/ii.py
@@ -0,0 +1,27 @@
+#True False
+
+
+# abc=True
+# xyz=False
+# if abc and xyz:
+#     print("123")
+# elif abc or xyz:
+#     print("iii")
+
+
+
+# is_logged_in=False
+# is_admin=True
+# if is_logged_in and is_admin:
+#     print("Adm")
+# elif is_logged_in:
+#     print("Abc")
+# else:
+#     print("Hi hi hi")
+
+
+
+# nimadrlar=[1,2,3,"Xyz","Abc","Iii"]
+# if "Xyziii" not in nimadrlar:
+#     print("Hi hi hi")
+
