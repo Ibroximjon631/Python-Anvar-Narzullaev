c60487d6e7a98047d9611a9a1f32e3abb853f43d
ibroximjon631 <ibroximjon631@gmail.com>
2025-04-25 15:42:40 +0500

    iii

--- Python/Pythoniiiii/Dict.py
+++ Python/Pythoniiiii/Dict.py
@@ -1,9 +1,20 @@
 dicttt={
     'name':'Ibroximjon',
-    'age':20,
-    'nimadr':'Iiii'
+    'age':"20",
+    'nimadr':'Iiii',
+    'kimdr':'Ibroximjon'
 }
 #print(dicttt.items())
-for ii,iii in dicttt.items():
-    print(f"Ii: {ii},Iii: {iii}")
-print(dicttt.keys())
+# for ii,iii in dicttt.items():
+#     print(f"Ii: {ii},Iii: {iii}")
+# print(dicttt.keys())
+# for iiiiii in sorted(dicttt.values()):
+#     print(iiiiii)
+
+
+for abcd in set(dicttt.values()):
+    print(abcd)
+
+#2ta Ibroximjon boriydi Bittasini ciqaryapti
+abc={'Ibroximjon','Kimdr','Ibroximjon'}
+print(abc,type(abc))
